d4e919f4c87c645154869db8d3b890fedc8c75f4
Sm4rtBoyTom <155878804+Sm4rtBoyTom@users.noreply.github.com>
2026-01-05 17:32:06 +0100

    Add files via upload

---
+++ VisualStudio/StalkerAidsAndSupplementsUtils.cs
@@ -0,0 +1,46 @@
+﻿using Il2Cpp;
+using MelonLoader;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace StalkerAidsAndSupplementsMod
+{
+    internal static class StalkerAidsAndSupplementsUtils
+    {
+
+        public static Il2CppSystem.Collections.Generic.List<FoodItem.Nutrient> VitC(int amount = 10)
+        {
+            FoodItem.Nutrient vitc = new FoodItem.Nutrient();
+            vitc.m_Amount = amount;
+            vitc.m_Nutrient = new Il2CppTLD.Gameplay.AssetReferenceNutrientDefinition("13a8bda1e12982e428b7551cc01b01df");
+            Il2CppSystem.Collections.Generic.List<FoodItem.Nutrient> list = new Il2CppSystem.Collections.Generic.List<FoodItem.Nutrient>();
+            list.Add(vitc);
+            return list;
+        }
+        public static Material LiquidMaterial()
+        {
+            Texture2D LiquidTexture = StalkerAidAndSupplements.JamTexturesBundle.LoadAsset<Texture2D>("T_RosehipJam_Cooking");
+            Material LiquidMaterial = new Material(GearItem.LoadGearItemPrefab("GEAR_CoffeeCup").gameObject.GetComponent<Cookable>().m_CookingPotMaterialsList[0]);
+            LiquidMaterial.SetTexture("_Main_texture2", LiquidTexture);
+            return LiquidMaterial;
+        }
+
+        public static AssetBundle LoadFromStream(string name)
+        {
+            using (Stream? stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(name))
+            {
+                MemoryStream? memory = new((int)stream.Length);
+                stream!.CopyTo(memory);
+
+                Il2CppSystem.IO.MemoryStream memoryStream = new Il2CppSystem.IO.MemoryStream(memory.ToArray());
+
+                AssetBundle loadFromMemoryInternal = AssetBundle.LoadFromStream(memoryStream);
+                return loadFromMemoryInternal;
+            };
+        }
+
+
+    }
+}
+
+
